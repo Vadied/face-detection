06a44a6df193496c4712f89c0875e17fd7431755
davide.filippi <davide.filippi@coolshop.it>
2021-02-24 17:02:53 +0100

    connect to backend

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,38 +1,92 @@
-import React from "react";
+import React, { Component } from "react";
 import { Form, Button } from "react-bootstrap";
 import "./_register.scss";
 
 import { routes } from "../../Constants";
 
-const Register = ({ onRouteChange }) => {
-  return (
-    <div className="register">
-      <Form>
-        <Form.Group controlId="name">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="email" placeholder="Enter name" />
-        </Form.Group>
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      registerName: "",
+      registerEmail: "",
+      registerPassword: "",
+    };
+  }
 
-        <Form.Group controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
+  onSubmitRegister = () => {
+    const params = {
+      email: this.state.registerEmail,
+      password: this.state.registerPassword,
+      name: this.state.registerName,
+    };
 
-        <Form.Group controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
+    const data = {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(params),
+    };
 
-        <Button
-          variant="primary"
-          type="submit"
-          onClick={() => onRouteChange(routes.home)}
-        >
-          Register
-        </Button>
-      </Form>
-    </div>
-  );
-};
+    fetch("http://localhost:3000/register", data).then((user) => {
+      this.props.loadUser(user);
+      this.props.onRouteChange(routes.home);
+    });
+  };
+
+  onNameChange = (event) => {
+    this.setState({ registerName: event.target.value });
+  };
+
+  onEmailChange = (event) => {
+    this.setState({ registerEmail: event.target.value });
+  };
+
+  onPasswordChange = (event) => {
+    this.setState({ registerPassword: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="register">
+        <Form>
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter name"
+              onChange={this.onNameChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={this.onEmailChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={this.onPasswordChange}
+            />
+          </Form.Group>
+
+          <Button
+            variant="primary"
+            type="submit"
+            onClick={this.onSubmitRegister}
+          >
+            Register
+          </Button>
+        </Form>
+      </div>
+    );
+  }
+}
 
 export default Register;
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,38 +1,78 @@
-import React from "react";
+import React, { Component } from "react";
 import { Form, Button } from "react-bootstrap";
 import "./_signIn.scss";
 
 import { routes } from "../../Constants";
 
-const SignIn = ({ onRouteChange }) => {
-  return (
-    <div className="sign-in">
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: "",
+      signInPassword: "",
+    };
+  }
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button
-          variant="primary"
-          type="submit"
-          onClick={() => onRouteChange(routes.home)}
-        >
-          Sign In
-        </Button>
+  onSubmitSignIn = () => {
+    const params = {
+      email: this.state.signInEmail,
+      password: this.state.signInPassword,
+    };
 
-        <Form.Group controlId="formRegister">
-          <Form.Text onClick={() => onRouteChange(routes.register)}>
-            Register
-          </Form.Text>
-        </Form.Group>
-      </Form>
-    </div>
-  );
-};
+    const data = {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(params),
+    };
+    fetch("http://localhost:3000/signin", data).then((user) => {
+      this.props.loadUser(user);
+      this.props.onRouteChange(routes.home);
+    });
+  };
+
+  onEmailChange = (event) => {
+    this.setState({ signInEmail: event.target.value });
+  };
+
+  onPasswordChange = (event) => {
+    this.setState({ signInPassword: event.target.value });
+  };
+
+  render() {
+    const { onRouteChange } = this.props;
+    return (
+      <div className="sign-in">
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={this.onEmailChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={this.onPasswordChange}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" onClick={this.onSubmitSignIn}>
+            Sign In
+          </Button>
+
+          <Form.Group controlId="formRegister">
+            <Form.Text onClick={() => onRouteChange(routes.register)}>
+              Register
+            </Form.Text>
+          </Form.Group>
+        </Form>
+      </div>
+    );
+  }
+}
 
 export default SignIn;
